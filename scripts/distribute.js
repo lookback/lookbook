5990333982c3d9e44de7a01e442151ec953a2c84
Johan Brook <johan@lookback.io>
2019-03-21 22:42:34 +0100

    Make distribution script always put latest lookbook.css in `latest` prefix on S3

--- scripts/distribute.js
+++ scripts/distribute.js
@@ -5,18 +5,56 @@ const fs = require('fs');
 const { promisify } = require('util');
 const dotenv = require('dotenv');
 const { version: pkgVersion } = require('../package.json');
+const { colorize, Color, link } = require('../lib/colorize');
+
+/*
+    DISTRIBUTION SCRIPT FOR LOOKBOOK
+    -----------------------------------
+
+    * This script will release a given CSS file to a set S3 bucket.
+    * All releases are immutable, i.e. they will refuse to overwrite
+      an existing version in the bucket.
+    * Versions are decided by the `version` field in `package.json`.
+    * The script *will* overwrite the `latest` prefix in the bucket with
+      the given CSS file.
+
+
+    USAGE
+    -----------------------------------
+    ```
+    node scripts/distribute.js <path to .css file>
+    ```
+    
+    CREDENTIALS
+    -----------------------------------
+    This script needs the following as environment variables:
+    
+    * `AWS_ACCESS_KEY_ID`
+    * `AWS_SECRET_ACCESS_KEY`
+*/
 
 dotenv.load();
+
+const printError = (...args) => console.error(colorize(Color.Red, ...args));
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const bucket = process.env.AWS_BUCKET || 'lookbook.lookback.io';
 const region = 'eu-west-1';
 
+const LATEST_PREFIX_NAME = 'latest';
+
 if (!accessKeyId || !secretAccessKey) {
-    console.error(
+    printError(
         '❌ Missing AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY as env variables'
     );
+    process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+if (!filePath) {
+    printError('❌ Please specify path to file as first argument');
     process.exit(1);
 }
 
@@ -56,44 +94,51 @@ const stylesheetExistsInBucket = (filename, version) =>
             throw err;
         });
 
-const uploadFile = async (pathToFile) => {
-    console.log('-> Releasing to S3...');
-
-    const version = pkgVersion;
-    const contents = await readFile(pathToFile);
-    const filename = path.basename(pathToFile);
-    const key = await makeKeyName(filename, version);
-
+const uploadToBucket = async ({ contents, key, s3opts = {} }) => {
     const opts = {
         Bucket: bucket,
         Key: key,
         ContentType: 'text/css',
         ACL: 'public-read',
         Body: contents,
+        ...s3opts,
     };
 
+    await putObject(opts);
+};
+
+const releaseFile = async (pathToFile) => {
+    console.log(colorize(Color.Dim, '-> Releasing to S3...\n'));
+
+    const version = pkgVersion;
+    const contents = await readFile(pathToFile);
+    const filename = path.basename(pathToFile);
+    const versionedKey = await makeKeyName(filename, version);
+    const latestKey = await makeKeyName(filename, LATEST_PREFIX_NAME);
+
     if (await stylesheetExistsInBucket(filename, version)) {
-        console.error(
+        printError(
             `❌ Version ${version} already exists on S3.
-Please bump the version in package.json and re-run.`
+Please bump the version in package.json and re-run. Nothing has changed on S3 at this point.`
         );
         process.exit(1);
     }
 
-    await putObject(opts);
+    // Put new versioned file
+    await uploadToBucket({ contents, key: versionedKey });
 
-    return `✅ Released ${version} to\nhttps://s3.${region}.amazonaws.com/${
-        opts.Bucket
-    }/${opts.Key}`;
+    // Overwrite latest
+    await uploadToBucket({
+        contents,
+        key: latestKey,
+    });
+
+    return `✅ ${colorize(Color.Green, `Released ${version} to`)}
+
+* ${link(`https://s3.${region}.amazonaws.com/${bucket}/${versionedKey}`)}
+* ${link(`https://s3.${region}.amazonaws.com/${bucket}/${latestKey}`)}`;
 };
 
-const filePath = process.argv[2];
-
-if (!filePath) {
-    console.error('❌ Please specify path to file as first argument');
-    process.exit(1);
-}
-
-uploadFile(filePath)
+releaseFile(filePath)
     .then((res) => console.log(res))
-    .catch((err) => console.error('❌ There was an error:\n', err.message));
+    .catch((err) => printError('❌ There was an error:\n', err.message));
