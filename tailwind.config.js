a79f843dfd2b2581a945c58c924ec390a4bbfe43
Johan Brook <johan@lookback.io>
2019-03-30 19:02:14 +0100

    Fix box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -664,14 +664,12 @@ module.exports = {
         |
     */
 
-        boxShadow: {
-            default: '0 2px 4px 0 rgba(0,0,0,0.10)',
-            md: '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
-            lg: '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
-            inner: 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
-            outline: '0 0 0 3px rgba(52,144,220,0.5)',
+        boxShadow: (theme) => ({
+            default: `rgba(${theme('colors.grey-30')}, .5) 0 4px 25px`,
+            large: `rgba(${theme('colors.grey-30')}, .6) 0 6px 45px`,
+            small: `rgba(${theme('colors.grey-30')}, .3) 0 2px 10px`,
             none: 'none',
-        },
+        }),
 
         /*
         |-----------------------------------------------------------------------------
