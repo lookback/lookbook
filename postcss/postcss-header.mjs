ff1213d30557ccc265bb346bb1e16d3e4974e0e1
Johan Brook <johan@lookback.io>
2025-06-18 14:31:37 +0200

    Use ESM postcss config, move plugins

--- postcss/postcss-header.mjs
+++ postcss/postcss-header.mjs
@@ -1,5 +1,5 @@
 // Inlined from https://github.com/fengyuanchen/postcss-header (MIT)
-const header = (options = {}) => {
+export default (options = {}) => {
   options = Object.assign(
     {
       header: '',
@@ -26,5 +26,4 @@ const header = (options = {}) => {
   };
 };
 
-module.exports = header;
-module.exports.postcss = true;
+export const postcss = true;
