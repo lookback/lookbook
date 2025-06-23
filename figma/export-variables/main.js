75c264f2bc641145560f6f6b5bbd3c3ee2c8abe7
Johan Brook <johan@lookback.io>
2025-06-23 09:40:24 +0200

    Generate raw Figma variables to gen/figma-variables.json
    Try not to do any mappings or transformations in here. Let downstream
    scripts to that, such as the PostCSS plugin.

--- figma/export-variables/main.js
+++ figma/export-variables/main.js
@@ -54,6 +54,7 @@ async function tokenOf(v) {
     name: nameOf(v),
     value: res.value,
     target: res.target,
+    type: v.resolvedType,
     description: v.description || null,
   };
 }
@@ -98,26 +99,9 @@ async function transformValue(v) {
   }
 
   return {
-    value: valueOf(v, value),
+    value,
     target: null,
   };
-}
-
-/**
- * @param {Variable} v
- * @param {VariableValue} value
- */
-function valueOf(v, value) {
-  switch (v.resolvedType) {
-    case 'COLOR':
-      return rgbaToLch(value);
-    case 'FLOAT': {
-      return parseFloat(value.toFixed(3));
-    }
-    case 'BOOLEAN':
-    case 'STRING':
-      return value;
-  }
 }
 
 /**
@@ -125,46 +109,6 @@ function valueOf(v, value) {
  */
 function findValue(values) {
   return Object.values(values)[0];
-}
-
-/**
- * @param {RGBA | RGB} rgba
- */
-function rgbaToLch(rgba) {
-  const srgbToLinear = (c) =>
-    c <= 0.04045 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
-
-  const { r, g, b, a: alpha } = rgba;
-
-  // RGB => linear RGB
-  const rLin = srgbToLinear(r);
-  const gLin = srgbToLinear(g);
-  const bLin = srgbToLinear(b);
-
-  // linear RGB => OKLAB
-  const l = 0.412165612 * rLin + 0.536275208 * gLin + 0.0514575653 * bLin;
-  const m = 0.211859107 * rLin + 0.6807189584 * gLin + 0.107406579 * bLin;
-  const s = 0.0883097947 * rLin + 0.2818474174 * gLin + 0.6299787005 * bLin;
-
-  const l_ = Math.cbrt(l);
-  const m_ = Math.cbrt(m);
-  const s_ = Math.cbrt(s);
-
-  const L = 0.2104542553 * l_ + 0.793617785 * m_ - 0.0040720468 * s_;
-  const a = 1.9779984951 * l_ - 2.428592205 * m_ + 0.4505937099 * s_;
-  const b_ = 0.0259040371 * l_ + 0.7827717662 * m_ - 0.808675766 * s_;
-
-  // OKLAB => OKLCH
-  const C = Math.sqrt(a * a + b_ * b_);
-  let H = Math.atan2(b_, a) * (180 / Math.PI);
-  if (H < 0) H += 360;
-
-  // Handle achromatic colors (when chroma is very close to 0)
-  if (C < 0.0001) H = 0;
-
-  return alpha != null && alpha != 1
-    ? `oklch(${L} ${C} ${H} / ${alpha})`
-    : `oklch(${L} ${C} ${H})`;
 }
 
 // Create an invisible UI that triggers the download of the JSON file.
--- postcss/postcss-figma-variables.js
+++ postcss/postcss-figma-variables.js
@@ -2,10 +2,11 @@ const json = require('../gen/figma-variables.json');
 
 /**
  * @typedef {object} FigmaVariable
- * @property {string} name - The name of the variable.
- * @property {string | number} value - The value of the variable.
- * @property {string | null} [target] - The target of the variable, if applicable.
- * @property {string | null} [description] - The description of the variable, if available.
+ * @property {string} name The name of the variable.
+ * @property {VariableResolvedDataType} type The type of the variable.
+ * @property {VariableValue} value The value of the variable.
+ * @property {string | null} [target] The target of the variable, if applicable.
+ * @property {string | null} [description] The description of the variable, if available.
  */
 
 /** Add Figma variables as Tailwind @theme variables.
@@ -13,7 +14,7 @@ const json = require('../gen/figma-variables.json');
  * @return {import('postcss').Plugin}
  */
 const plugin = () => {
-  const { tokens, semantic, primitives } = json;
+  const { theme, semantic, primitives } = json;
 
   return {
     postcssPlugin: 'postcss-tokens',
@@ -45,7 +46,7 @@ const plugin = () => {
         atRule.replaceWith([
           ...declOf(primitives),
           ...declOf(semantic),
-          ...declOf(tokens),
+          ...declOf(theme),
         ]);
       },
     },
@@ -66,24 +67,24 @@ const FONT_WEIGHT_MAP = {
 };
 
 /**
- * @param {FigmaVariable} t
+ * @param {FigmaVariable} v
  * @param {import('postcss').Result} result
  */
-const mapVariable = (t, result) => {
+const mapVariable = (v, result) => {
   // If the variable is an alias, use the target name instead of the raw value.
-  if (t.target) return `var(--${t.target})`;
+  if (v.target) return `var(--${v.target})`;
 
-  if (t.name.startsWith('font-weight')) {
-    const weight = FONT_WEIGHT_MAP[t.value.toLowerCase()];
+  if (v.name.startsWith('font-weight')) {
+    const weight = FONT_WEIGHT_MAP[v.value.toLowerCase()];
     if (!weight) {
-      result.warn(`Unknown font weight "${t.value}" for variable "${t.name}".`);
-      return t.value;
+      result.warn(`Unknown font weight "${v.value}" for variable "${v.name}".`);
+      return v.value;
     }
 
     return weight;
   }
 
-  return t.value + unitOf(t);
+  return valueOf(v) + unitOf(v);
 };
 
 const UNIT_LESS = ['leading'];
@@ -99,6 +100,62 @@ const unitOf = (t) => {
   }
 
   return '';
+};
+
+/**
+ * @param {FigmaVariable} v
+ */
+const valueOf = (v) => {
+  switch (v.type) {
+    case 'COLOR':
+      return rgbaToLch(v.value);
+    case 'FLOAT': {
+      return parseFloat(v.value.toFixed(3));
+    }
+    case 'BOOLEAN':
+    case 'STRING':
+      return v.value;
+  }
+};
+
+/**
+ * @param {RGBA | RGB} rgba
+ */
+const rgbaToLch = (rgba) => {
+  const srgbToLinear = (c) =>
+    c <= 0.04045 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
+
+  const { r, g, b, a: alpha } = rgba;
+
+  // RGB => linear RGB
+  const rLin = srgbToLinear(r);
+  const gLin = srgbToLinear(g);
+  const bLin = srgbToLinear(b);
+
+  // linear RGB => OKLAB
+  const l = 0.412165612 * rLin + 0.536275208 * gLin + 0.0514575653 * bLin;
+  const m = 0.211859107 * rLin + 0.6807189584 * gLin + 0.107406579 * bLin;
+  const s = 0.0883097947 * rLin + 0.2818474174 * gLin + 0.6299787005 * bLin;
+
+  const l_ = Math.cbrt(l);
+  const m_ = Math.cbrt(m);
+  const s_ = Math.cbrt(s);
+
+  const L = 0.2104542553 * l_ + 0.793617785 * m_ - 0.0040720468 * s_;
+  const a = 1.9779984951 * l_ - 2.428592205 * m_ + 0.4505937099 * s_;
+  const b_ = 0.0259040371 * l_ + 0.7827717662 * m_ - 0.808675766 * s_;
+
+  // OKLAB => OKLCH
+  const C = Math.sqrt(a * a + b_ * b_);
+  let H = Math.atan2(b_, a) * (180 / Math.PI);
+  if (H < 0) H += 360;
+
+  // Handle achromatic colors (when chroma is very close to 0)
+  if (C < 0.0001) H = 0;
+
+  return alpha != null && alpha != 1
+    ? `oklch(${L} ${C} ${H} / ${alpha})`
+    : `oklch(${L} ${C} ${H})`;
 };
 
 plugin.postcss = true;
